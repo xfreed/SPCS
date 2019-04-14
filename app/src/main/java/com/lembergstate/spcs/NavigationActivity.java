e7131c6f32c953cd6fb50f587aaf1fc67c913bce
Michael Kunynets <skilledaligator@gmail.com>
2019-04-14 23:30:55 +0300

    Update
    Drawable xml's file (home,bell,person,info)

--- app/src/main/java/com/lembergstate/spcs/NavigationActivity.java
+++ app/src/main/java/com/lembergstate/spcs/NavigationActivity.java
@@ -13,16 +13,16 @@ public class NavigationActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_info:
+                    mTextMessage.setText(R.string.title_info);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_children:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
@@ -38,8 +38,8 @@ public class NavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
