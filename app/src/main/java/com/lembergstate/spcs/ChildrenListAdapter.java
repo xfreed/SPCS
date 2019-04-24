d160ec2a70c16898739ac4afea09f85b4ce9bb83
Michael Kunynets <skilledaligator@gmail.com>
2019-04-24 19:49:25 +0300

    Update
    -Added Children list adapter
    -Added Children list xml
    -Changed navigation Activity

---
+++ app/src/main/java/com/lembergstate/spcs/ChildrenListAdapter.java
@@ -0,0 +1,47 @@
+package com.lembergstate.spcs;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ChildrenListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] childName;
+    private final String[] date;
+    private final String[] time;
+    private final String[] inOrOut;
+
+
+    public ChildrenListAdapter(Activity context, String[] childName, String[] date, String[] time, String[] inOrOut) {
+        super(context,R.layout.childrenlist,childName);
+        this.context = context;
+        this.childName = childName;
+        this.date = date;
+        this.time = time;
+        this.inOrOut = inOrOut;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.childrenlist, null,true);
+
+        TextView childname = rowView.findViewById(R.id.ChildName);
+        ImageView threedots = rowView.findViewById(R.id.threeDots);
+        TextView Tdate = rowView.findViewById(R.id.LastDate);
+        TextView Ttime = rowView.findViewById(R.id.LastTime);
+        TextView TinOrOut = rowView.findViewById(R.id.InOrOut);
+        childname.setText(childName[position]);
+        threedots.setImageResource(R.drawable.ic_more_horiz_white_24dp);
+        Tdate.setText(date[position]);
+        Ttime.setText(time[position]);
+        TinOrOut.setText(inOrOut[position]);
+
+        return rowView;
+
+    }
+}
--- app/src/main/java/com/lembergstate/spcs/NavigationActivity.java
+++ app/src/main/java/com/lembergstate/spcs/NavigationActivity.java
@@ -2,16 +2,42 @@ package com.lembergstate.spcs;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class NavigationActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private ListView ChildList;
+
+    String[] ChildName = {
+            "Aragorn", "Crampus",
+            "Protps", "Artos",
+            "Shong",
+    };
+
+    String[] date = {
+            "Today", "Today",
+            "yesterday", "Today",
+            "yesterday",
+    };
+    String[] time = {
+            "8:20", "8:25",
+            "8:27", "8:31",
+            "8:26",
+    };
+    String[] InOrOut = {
+            "In", "In",
+            "In", "Out",
+            "In",
+    };
 
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,6 +67,36 @@ public class NavigationActivity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ChildrenListAdapter adapter = new ChildrenListAdapter(this,ChildName,date,time,InOrOut);
+        ChildList = findViewById(R.id.ChilderList);
+        ChildList.setAdapter(adapter);
+        ChildList.setOnItemClickListener((parent, view, position, id) -> {
+            // TODO Auto-generated method stub
+            if(position == 0) {
+                //code specific to first list item
+                Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+            }
+
+            else if(position == 1) {
+                //code specific to 2nd list item
+                Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+            }
+
+            else if(position == 2) {
+
+                Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+            }
+            else if(position == 3) {
+
+                Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+            }
+            else if(position == 4) {
+
+                Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 
 }
